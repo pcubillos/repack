d9c7e5866249a9d0537afe71e78f2998741862ef
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-09-25 13:43:02 +0200

    Added __version__ variable to package, intro print statement, and time stamps.

--- repack.py
+++ repack.py
@@ -5,8 +5,9 @@
 
 import sys
 import warnings
+import time
 
-from repack import *
+import repack as rep
 
 if __name__ == "__main__":
   warnings.simplefilter("ignore", RuntimeWarning)
@@ -14,6 +15,15 @@ if __name__ == "__main__":
     print("\n{:s}\n  Error: Wrong usage.\n{:s}\n".format(70*":", 70*":"))
     sys.exit(0)
 
+  print("\n{:s}\n"
+        "  repack: line-transition data compression.\n"
+        "  Version {:s}.\n"
+        "  Copyright (c) 2017 Patricio Cubillos.\n"
+        "  repack is open-source software under the MIT license.\n"
+        "{:s}\n\n".format(":"*70, rep.__version__, ":"*70))
+
+  print("Starting: {:s}".format(time.ctime()))
   cfile =  sys.argv[1]
-  args = parser(cfile)
-  repack(*args)
+  args = rep.parser(cfile)
+  rep.repack(*args)
+  print("End: {:s}".format(time.ctime()))
