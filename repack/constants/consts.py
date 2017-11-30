f822ada1e9f180c99b13a5792f9afa665ea1539e
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-11-30 13:12:45 +0100

    Added nano to the constants.

--- repack/constants/consts.py
+++ repack/constants/consts.py
@@ -3,7 +3,7 @@
 
 import scipy.constants as sc
 
-__all__ = ["kB", "amu", "e", "me", "N0", "C1", "C2", "C3"]
+__all__ = ["kB", "amu", "e", "me", "N0", "nano", "C1", "C2", "C3"]
 
 # Boltzmann constant in erg K-1:
 kB  = sc.k * 1e7
@@ -16,6 +16,9 @@ me = sc.m_e * 1e3
 # Amagat in molecules cm-3:
 N0 = sc.physical_constants[
         "Loschmidt constant (273.15 K, 101.325 kPa)"][0] * 1e-6
+
+# One nanometer in centimeters:
+nano = 1e-7
 
 # Other constructed constants:
 C1  = 4.0 * sc.epsilon_0 * sc.m_e * sc.c**2 / sc.e**2 * 0.01  # cm-1
